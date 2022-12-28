026cb2897d8dc535f805e31b1db649c9d6bbd36e
nxf77715 <shrivatsa.hande@nxp.com>
2022-12-28 18:30:26 +0530

    Weaver HAL AIDL
    - Weaver HAL migration from HIDL to AIDL.
    
    Change-Id: I2f041e112ddca8f3365badf5f4368113ddaf51bd

---
+++ weaver/aidl_impl/Weaver.cpp
@@ -0,0 +1,140 @@
+/******************************************************************************
+ *
+ *  Copyright 2023 NXP
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+#include "Weaver.h"
+#include <log/log.h>
+
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace weaver {
+
+using ::ndk::ScopedAStatus;
+using std::vector;
+
+// Methods from ::android::hardware::weaver::IWeaver follow.
+
+Weaver::Weaver() {
+  ALOGD("Weaver Constructor");
+  pInterface = WeaverImpl::getInstance();
+  if (pInterface != NULL) {
+    pInterface->Init();
+  } else {
+    ALOGE("Failed to get Weaver Interface");
+  }
+}
+
+ScopedAStatus Weaver::getConfig(WeaverConfig *out_config) {
+  ALOGD("Weaver::getConfig");
+
+  if (out_config == NULL) {
+    ALOGE("Invalid param");
+    return ScopedAStatus::fromServiceSpecificErrorWithMessage(
+        STATUS_FAILED, "Null pointer passed");
+  }
+  memset(out_config, 0, sizeof(WeaverConfig));
+
+  if (pInterface == NULL) {
+    return ScopedAStatus::fromServiceSpecificErrorWithMessage(
+        STATUS_FAILED, "Weaver interface not defined");
+  }
+  SlotInfo slotInfo;
+  Status_Weaver status = pInterface->GetSlots(slotInfo);
+  if (status == WEAVER_STATUS_OK) {
+    out_config->slots = slotInfo.slots;
+    out_config->keySize = slotInfo.keySize;
+    out_config->valueSize = slotInfo.valueSize;
+    ALOGV("Weaver Success for getSlots Slots :(%d)", out_config->slots);
+    return ScopedAStatus::ok();
+  } else {
+    return ScopedAStatus::fromServiceSpecificErrorWithMessage(
+        STATUS_FAILED, "Failed to retreive slots info");
+  }
+}
+
+ScopedAStatus Weaver::read(int32_t in_slotId, const vector<uint8_t> &in_key,
+                           WeaverReadResponse *out_response) {
+
+  ALOGV("Weaver::read");
+  if (in_key.empty() || out_response == NULL) {
+    return ScopedAStatus::fromServiceSpecificErrorWithMessage(
+        STATUS_FAILED, "Null pointer passed");
+  }
+  if (pInterface == NULL) {
+    return ScopedAStatus::fromServiceSpecificErrorWithMessage(
+        STATUS_FAILED, "Weaver interface not defined");
+  }
+  memset(out_response, 0, sizeof(WeaverReadResponse));
+
+  ReadRespInfo readInfo;
+  ScopedAStatus retStatus;
+  Status_Weaver status;
+
+  status = pInterface->Read(in_slotId, in_key, readInfo);
+  switch (status) {
+  case WEAVER_STATUS_OK:
+    ALOGV("Read OK");
+    out_response->value = readInfo.value;
+    retStatus = ScopedAStatus::ok();
+    break;
+  case WEAVER_STATUS_INCORRECT_KEY:
+    ALOGE("Read Incorrect Key");
+    out_response->value.resize(0);
+    out_response->timeout = readInfo.timeout;
+    retStatus = ScopedAStatus::fromServiceSpecificErrorWithMessage(
+        STATUS_INCORRECT_KEY, "Incorrect key passed");
+    break;
+  case WEAVER_STATUS_THROTTLE:
+    ALOGE("Read WEAVER_THROTTLE");
+    out_response->value.resize(0);
+    out_response->timeout = readInfo.timeout;
+    retStatus = ScopedAStatus::fromServiceSpecificErrorWithMessage(
+        STATUS_THROTTLE, "Throttle active");
+    break;
+  default:
+    out_response->timeout = 0;
+    out_response->value.resize(0);
+    retStatus = ScopedAStatus::fromServiceSpecificErrorWithMessage(
+        STATUS_FAILED, "Unknown error");
+    break;
+  }
+  return retStatus;
+}
+
+ScopedAStatus Weaver::write(int32_t in_slotId, const vector<uint8_t> &in_key,
+                            const vector<uint8_t> &in_value) {
+  ALOGV("Weaver::write");
+  if (in_key.empty() || in_value.empty()) {
+    return ScopedAStatus::fromServiceSpecificErrorWithMessage(
+        STATUS_FAILED, "Invalid parameters passed");
+  }
+  if (pInterface == NULL) {
+    return ScopedAStatus::fromServiceSpecificErrorWithMessage(
+        STATUS_FAILED, "Weaver interface not defined");
+  }
+  if (pInterface->Write(in_slotId, in_key, in_value) == WEAVER_STATUS_OK) {
+    return ScopedAStatus::ok();
+  } else {
+    return ScopedAStatus::fromServiceSpecificErrorWithMessage(STATUS_FAILED,
+                                                              "Unknown error");
+  }
+}
+
+} // namespace weaver
+} // namespace hardware
+} // namespace android
+} // namespace aidl
