15222d6b03003d7ffb3a56df44e15b85cbd91a2e
nxf63690 <sourav.kumar@nxp.com>
2024-05-03 21:21:29 +0530

    Fix compilation failure for AIDL Hal transport
    Change-Id: Ie6bb353a11fa23f682ba7af715ce1b2e0c31121d

--- weaver/libese_weaver/inc/TransportFactory.h
+++ weaver/libese_weaver/inc/TransportFactory.h
@@ -30,24 +30,28 @@
  ** See the License for the specific language governing permissions and
  ** limitations under the License.
  **
- ** Copyright 2020-2023 NXP
+ ** Copyright 2020-2024 NXP
  **
  *********************************************************************************/
 
 #ifndef __SE_TRANSPORT_FACTORY__
 #define __SE_TRANSPORT_FACTORY__
 
-#include "HalToHalTransport.h"
+#ifdef OMAPI_TRANSPORT
 #include "OmapiTransport.h"
+#else
+#include "HalToHalTransport.h"
+#endif
 #include "SocketTransport.h"
 
 namespace se_transport {
 
-using keymint::javacard::HalToHalTransport;
 using keymint::javacard::ITransport;
 using keymint::javacard::SocketTransport;
 #ifdef OMAPI_TRANSPORT
 using keymint::javacard::OmapiTransport;
+#else
+using keymint::javacard::HalToHalTransport;
 #endif
 
 /**
